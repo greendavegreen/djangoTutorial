5dbe1f7066af26d07fb065b3027aee66d37b62ae
David Green <david.green@cd-adapco.com>
2017-06-06 11:30:58 -0400

    updated name of app user inside of postgres

--- djangoTutorial/settings.py
+++ djangoTutorial/settings.py
@@ -80,7 +80,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'mydatabase',
-        'USER': 'postgres',
+        'USER': 'myapp',
         'PASSWORD': 'mypassword',
         'HOST': '127.0.0.1',
         'PORT': '5432',
